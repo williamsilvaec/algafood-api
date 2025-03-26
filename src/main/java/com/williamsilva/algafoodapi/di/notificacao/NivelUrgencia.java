9b619c2f0d3c7fb8d74fcb50c2f17153f0ccc98b
William Silva <williamsilvaec@gmail.com>
2025-03-26 18:52:45 -0300

    limpar projeto

--- src/main/java/com/williamsilva/algafoodapi/di/notificacao/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.williamsilva.algafoodapi.di.notificacao;
-
-public enum NivelUrgencia {
-
-    URGENTE,
-    SEM_URGENCIA
-}
--- src/main/java/com/williamsilva/algafoodapi/di/notificacao/Notificador.java
+++
@@ -1,8 +0,0 @@
-package com.williamsilva.algafoodapi.di.notificacao;
-
-import com.williamsilva.algafoodapi.di.modelo.Cliente;
-
-public interface Notificador {
-
-    void notificar(Cliente cliente, String mensagem);
-}
