2adf4205b1adf209b294fa0c47ddcb021e82491d
William Silva <williamsilvaec@gmail.com>
2025-03-29 09:53:35 -0300

    Estender o JpaRepository para customizar o repositório base

--- src/main/java/com/williamsilva/algafoodapi/AlgafoodApiApplication.java
+++ src/main/java/com/williamsilva/algafoodapi/AlgafoodApiApplication.java
@@ -1,9 +1,12 @@
 package com.williamsilva.algafoodapi;
 
+import com.williamsilva.algafoodapi.infrastructure.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/williamsilva/algafoodapi/domain/repository/CozinhaRepository.java
+++ src/main/java/com/williamsilva/algafoodapi/domain/repository/CozinhaRepository.java
@@ -1,7 +1,6 @@
 package com.williamsilva.algafoodapi.domain.repository;
 
 import com.williamsilva.algafoodapi.domain.model.Cozinha;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+public interface CozinhaRepository extends CustomJpaRepository<Cozinha, Long> {
 }
